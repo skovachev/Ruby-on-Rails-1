5d613b16de7a78c5c325c89cca0c9a410d40a785
Stefan Kovachev <stefan.kovachev@gmail.com>
2015-03-19 21:17:29 +0200

    Improved rubocop rating

--- week1/1-sparse-matrix/solution.rb
+++ week1/1-sparse-matrix/solution.rb
@@ -2,26 +2,25 @@ class Array
   def compress
     table = []
     each do |row|
-      target_index = 0
+      target_idx = 0
       can_insert = false
 
       # figure out if it matches
       until can_insert
         can_insert = true
 
-        row.each_with_index do |value, column_index|
-          table_value = table[target_index + column_index]
+        row.each_with_index do |value, column_idx|
+          table_value = table[target_idx + column_idx]
           if !table_value.nil? && !value.nil?
-            target_index += 1; can_insert = false
-            break
+            target_idx += 1
+            can_insert = false
           end
         end
       end
 
       # insert when matching positions are found
-      row.each_with_index do |value, column_index|
-        table_index = target_index + column_index
-        table[table_index] = [column_index, value] unless value.nil?
+      row.each_with_index do |value, column_idx|
+        table[target_idx + column_idx] = [column_idx, value] unless value.nil?
       end
     end
     table

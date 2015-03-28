462fd8ec68b175ac6e44c2b7b3c7dff3ab959f27
Stefan Kovachev <stefan.kovachev@gmail.com>
2015-03-28 14:06:34 +0200

    Improved SQLite related code

--- week2/microblog/app/post.rb
+++ week2/microblog/app/post.rb
@@ -3,31 +3,28 @@ require_relative 'tag'
 class Post
   
   class << self
-    def find_by_id(id)
-      row = $db.execute("select id, title, content from posts where id=?", [id]).first
+    def build_from_db(row)
       post = Post.new(row[1], row[2], row[0]) unless row.nil?
       # get tags for post
       unless post.nil?
-        post.tags = Tag.find_for_post(post.id).map do |tag|
-          tag.name
-        end
+        post.tags = Tag.find_for_post(post.id).map(&:name)
       end
       post
     end
 
+    def find_by_id(id)
+      row = $db.get_first_row("SELECT id, title, content FROM posts WHERE id=?", [id])
+      Post.build_from_db row
+    end
+
     def find_all
-      $db.execute("select id, title, content from posts").map do |row|
-        post = Post.new(row[1], row[2], row[0])
-          # get tags for post
-        post.tags = Tag.find_for_post(post.id).map do |tag|
-          tag.name
-        end
-        post
+      $db.execute("SELECT id, title, content FROM posts").map do |row|
+        Post.build_from_db row
       end
     end
 
     def delete_by_id(id)
-      $db.execute("delete from posts where id=?", [id])
+      $db.execute("DELETE FROM posts WHERE id=?", [id])
     end
 
     def find_by_tag(tag)
@@ -38,24 +35,20 @@ class Post
         WHERE tags.name = ?
       EOT
       $db.execute(sql, [tag]).map do |row|
-        post = Post.new(row[1], row[2], row[0])
-        # get tags for post
-        post.tags = Tag.find_for_post(post.id).map do |t|
-          t.name
-        end
-        post
+        Post.build_from_db row
       end
     end
 
     def insert(post)
       data = [post.title, post.content]
 
-      # TODO must be in transaction
-      $db.execute("insert into posts(title, content) values ( ?, ? )", data)
-      post_id = $db.execute('SELECT last_insert_rowid()');
+      $db.transaction do 
+        $db.execute("INSERT INTO posts(title, content) VALUES (?, ?)", data)
+        post_id = $db.execute('SELECT last_insert_rowid()');
 
-      post.tags.each do |tag|
-        Tag.insert_if_not_exists tag, post_id
+        post.tags.each do |tag|
+          Tag.insert_if_not_exists tag, post_id
+        end
       end
     end
   end
--- week2/microblog/app/tag.rb
+++ week2/microblog/app/tag.rb
@@ -1,21 +1,25 @@
 class Tag
   
   class << self
+    def build_from_db row
+      Tag.new(row[1], row[0])
+    end
+
     def find_for_post(id)
-      $db.execute("select id, name from tags LEFT JOIN post_tags ON tags.id=post_tags.tag_id WHERE post_id=?", [id]).map do |row|
-        Tag.new(row[1], row[0])
+      $db.execute("SELECT id, name FROM tags LEFT JOIN post_tags ON tags.id=post_tags.tag_id WHERE post_id=?", [id]).map do |row|
+        Tag.build_from_db row
       end
     end
 
     def find_all
-      $db.execute("select id, name from tags").map do |row|
-        Tag.new(row[1], row[0])
+      $db.execute("SELECT id, name FROM tags").map do |row|
+        Tag.build_from_db row
       end
     end
 
     def insert_if_not_exists(tag_name, post_id)
-      tag = $db.execute("select 1 from tags WHERE name=?", [tag_name])
-      $db.execute("insert into tags(name) values ( ? )", [tag_name]) unless tag.present?
+      tag = $db.execute("SELECT 1 FROM tags WHERE name=?", [tag_name])
+      $db.execute("INSERT INTO tags(name) VALUES (?)", [tag_name]) unless tag.present?
       tag_id = tag.id if tag.present?
       unless tag.present?
         tag_id = $db.execute('SELECT last_insert_rowid()');

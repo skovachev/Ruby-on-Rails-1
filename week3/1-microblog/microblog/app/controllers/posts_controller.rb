b984d26a1d3f890f7ff53a596567262e8da15dc4
Stefan Kovachev <stefan.kovachev@gmail.com>
2015-04-09 00:49:58 +0300

    Rubocop ftw

--- week3/1-microblog/microblog/app/controllers/posts_controller.rb
+++ week3/1-microblog/microblog/app/controllers/posts_controller.rb
@@ -1,38 +1,25 @@
+# Posts controller
 class PostsController < ApplicationController
   def new
-
   end
 
   def create
     post = Post.create(post_params)
+    post_invalid = post.blank? || post.invalid?
 
-    puts post
-
-    unless post.blank? || post.invalid?
-
+    unless post_invalid
       # parse post tags
-      post.content.scan(/#(\w+)/).flatten.each do |tag|
-        t = Tag.find_by(name: tag)
-        if t.nil?
-          t = Tag.create(name: tag)
-        end
-        post.tags << t
-      end
-
+      process_post_tags post
       redirect_to action: 'show', id: post.id
     end
 
-    redirect_to controller: 'posts', action: 'new' if post.blank? || post.invalid?
+    redirect_to controller: 'posts', action: 'new' if post_invalid
   end
 
   def show
-
-    begin
-      @post = Post.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      redirect_to '/404'
-    end
-
+    @post = Post.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to '/404'
   end
 
   def destroy
@@ -44,5 +31,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def process_post_tags(post)
+    post.content.scan(/#(\w+)/).flatten.each do |tag|
+      t = Tag.find_by(name: tag)
+      t = Tag.create(name: tag) if t.nil?
+      post.tags << t
+    end
   end
 end

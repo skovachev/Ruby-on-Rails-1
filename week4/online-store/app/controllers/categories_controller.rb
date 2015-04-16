b43ee6f93746efdf32017648d222d0ac609eb1fb
Stefan Kovachev <stefan.kovachev@gmail.com>
2015-04-16 19:13:59 +0300

    Added controller and routes for categories | online store

--- week4/online-store/app/controllers/categories_controller.rb
+++ week4/online-store/app/controllers/categories_controller.rb
@@ -1,31 +1,44 @@
 # Categories controller
 class CategoriesController < ApplicationController
   def count 
-
+    Category.count
   end
 
   def index
-    
+    render json: Category.all
   end
 
   def get
-    @category = Category.find(params[:id])
+    render json: Category.find(params[:index])
   end
 
-  def range
-    
+  def range_offset_count
+    render json: Category.limit(params[:count]).offset(params[:index])
   end
 
-  def new
-    
+  def range_offset
+    render json: Category.offset(params[:index])
   end
 
-  #put
+  def create
+    render json: Category.create(category_params)
+  end
+
   def update
-      
+    category = Category.find(params[:index])
+    category.update(category_params)
+    render json: category
   end
 
   def destroy
-    
+    category = Category.find(params[:index])
+    category.destroy
+    render json: true
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
--- week4/online-store/config/routes.rb
+++ week4/online-store/config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
 
-  get 'brands/count', to: 'brands#count'
-  get 'brands/:index', to: 'brands#get'
-  get 'brands/range/:index/:count', to: 'brands#range_offset_count'
-  get 'brands/range/:index', to: 'brands#range_offset'
-  get 'brands', to: 'brands#index'
-  post 'brands/new', to: 'brands#create'
-  put 'brands/:index', to: 'brands#update'
-  delete 'brands/:index', to: 'brands#destroy'
+  # resource :brands
+  get       'brands/count',                 to: 'brands#count'
+  get       'brands/:index',                to: 'brands#get'
+  get       'brands/range/:index/:count',   to: 'brands#range_offset_count'
+  get       'brands/range/:index',          to: 'brands#range_offset'
+  get       'brands',                       to: 'brands#index'
+  post      'brands/new',                   to: 'brands#create'
+  put       'brands/:index',                to: 'brands#update'
+  delete    'brands/:index',                to: 'brands#destroy'
+
+  # resource :categories
+  get       'categories/count',                 to: 'categories#count'
+  get       'categories/:index',                to: 'categories#get'
+  get       'categories/range/:index/:count',   to: 'categories#range_offset_count'
+  get       'categories/range/:index',          to: 'categories#range_offset'
+  get       'categories',                       to: 'categories#index'
+  post      'categories/new',                   to: 'categories#create'
+  put       'categories/:index',                to: 'categories#update'
+  delete    'categories/:index',                to: 'categories#destroy'
 
 end
